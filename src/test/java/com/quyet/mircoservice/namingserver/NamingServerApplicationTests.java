e773776092665315e653f82f4071a28e232bfc76
bluerain4 <code.maito@gmail.com>
2021-09-27 09:21:58 +0700

    naming-server

---
+++ src/test/java/com/quyet/mircoservice/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quyet.mircoservice.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
