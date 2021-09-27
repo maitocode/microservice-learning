5ddb93a0ad6059951b93492f1bed33242b77e6b5
bluerain4 <code.maito@gmail.com>
2021-09-27 09:16:58 +0700

    currency-exchange-service

---
+++ src/test/java/com/quyet/microserive/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quyet.microserive.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
